0f3c21fb142c623923fc46fef0aa1148da2d1c30
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-11 21:54:41 +0000

    hobbies, birth place etc. added

--- directory.rb
+++ directory.rb
@@ -1,45 +1,66 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish just hit the return twice"
-  #creating an empty array
-  students = []
+  puts 'Please enter the names of the students'
+  puts 'To finish just hit the return twice'
   name = gets.chomp
-  #repeat this while name variable is empty
-    while !name.empty? do
-      #add the student hash to the array
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-      #get another name from the user
-      name = gets.chomp
-    end
-    #return the array of students
-    students
+
+  unless name.empty?
+    puts 'Please enter students hobby'
+    hobby = gets.chomp
+
+    puts 'Please enter students place of birth'
+    birth_place = gets.chomp
+
+    puts 'Please enter students height'
+    height = gets.chomp
+end
+  # repeat this while name variable is empty
+  # creating an empty array
+  students = []
+  until name.empty?
+    # add the student hash to the array
+    students << { name: name, cohort: :november, hobby: hobby,
+                  birth_place: birth_place, height: height }
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    puts 'Enter name'
+    name = gets.chomp
+
+    next if name.empty?
+    puts 'Enter hobby'
+    hobby = gets.chomp
+
+    puts 'Enter place of birth'
+    birth_place = gets.chomp
+
+    puts 'Enter height'
+    height = gets.chomp
+
+  end
+  # return the array of students
+  students
 end
 
-#prints the header of the list of students
+# prints the header of the list of students
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts 'The students of Villains Academy'
+  puts '-------------'
 end
-#prints the list of students in the array
+
+# prints the list of students in the array
 def print(students)
-  #students.each_with_index do |student, index|
-  #  puts "#{index+1}. #{student[:name]}\
-  #  (#{student[:cohort]} cohort)" if student[:name].length < 12
-  #end
   x = 0
   while x < students.length
 
     if students[x][:name].length < 12
-    puts "#{x+1}. #{students[x][:name]}\
-    (#{students[x][:cohort]} cohort)"
+      puts "#{x + 1}. #{students[x][:name]} (cohort: #{students[x][:cohort]}
+      hobby: #{students[x][:hobby]} birth place: #{students[x][:birth_place]}
+      height: #{students[x][:height]} )"
     end
     x += 1
   end
-
 end
 
-#prints a footer with the total student count
+# prints a footer with the total student count
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
